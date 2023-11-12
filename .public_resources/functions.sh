e5f889f1cd1b7ff77914638c2a89c8d4b0e750d1
iwantmyhatback <iwantmyhatbackk@gmail.com>
2023-11-12 18:34:30 -0500

    verbosity

--- .public_resources/functions.sh
+++ .public_resources/functions.sh
@@ -11,33 +11,33 @@ printf '%s\n' '[INFO] Loading public::functions'
 ###-----------------------------------------------------------------------------------------###
 
 add_path_item() {
-	usage="Usage: add_path_item [-f] [-b] [-n] [-q] DIR
+	USAGE="Usage: add_path_item [-f] [-b] [-n] [-q] DIR
 	-f Force any instance of DIR to the front of the PATH
 	-b Force any instance of DIR to the back of the PATH
 	-n Nonexistent directories do not return an error status
 	-q Quiet mode"
 
-	to_front=false
-	to_back=false
-	err_code=1
-	quiet_echo='echo'
+	TO_FRONT=false
+	TO_BACK=false
+	ERROR_CODE=1
+	QUIET_ECHO='echo'
 
 	while [ "$#" -gt 0 ]; do
 		case "$1" in
 			-f)
-				to_front=true
+				TO_FRONT=true
 				shift
 				;;
 			-b)
-				to_back=true
+				TO_BACK=true
 				shift
 				;;
 			-n)
-				err_code=0
+				ERROR_CODE=0
 				shift
 				;;
 			-q)
-				quiet_echo=':'
+				QUIET_ECHO=':'
 				shift
 				;;
 			*)
@@ -47,41 +47,41 @@ add_path_item() {
 	done
 
 	if [ -z "$1" ]; then
-		printf "\n%s\n" "$usage" 1>&2
+		printf "\n%s\n" "$USAGE" 1>&2
 		return 1
 	fi
 
-	if [ "${to_front}" = 'true' ] && [ "${to_back}" = 'true' ]; then
-		printf "\n%s\n" "$usage" 1>&2;
+	if [ "${TO_FRONT}" = 'true' ] && [ "${TO_BACK}" = 'true' ]; then
+		printf "\n%s\n" "$USAGE" 1>&2;
 		return 1
 	fi
 
 	if [ ! -d "$1" ]; then
-		$quiet_echo "$1 is not a directory." 1>&2
-		return $err_code
+		$QUIET_ECHO "$1 is not a directory." 1>&2
+		return $ERROR_CODE
 	fi
 
-	dir="${1}"
+	NEW_PATH_ITEM="${1}"
 
 	case :${PATH}: in
-		*:"$dir":*)
-			if [ ! "${to_front}" = 'true' ] || [ ! "${to_back}" = 'true' ]; then
-				$quiet_echo "${dir} already in path." 1>&2
+		*:"$NEW_PATH_ITEM":*)
+			if [ ! "${TO_FRONT}" = 'true' ] || [ ! "${TO_BACK}" = 'true' ]; then
+				$QUIET_ECHO "${NEW_PATH_ITEM} already in path." 1>&2
 				return 0
 			fi
-			PATH="${PATH#$dir:}"        # remove if at the start
-			PATH="${PATH%:$dir}"        # remove if at the end
-			PATH="${PATH//:$dir:/:}"    # remove if in the middle
+			PATH="${PATH#$NEW_PATH_ITEM:}"        # remove if at the start
+			PATH="${PATH%:$NEW_PATH_ITEM}"        # remove if at the end
+			PATH=$(printf "%s" "$PATH" | sed "s|:$NEW_PATH_ITEM:|:|g")    # remove if in the middle
 	esac
 
-	if [ ! "${to_front}" = 'true' ]; then
-		$quiet_echo "moved ${dir} to front of path." 1>&2
-		export PATH="${dir}:${PATH}"
+	if [ ! "${TO_FRONT}" = 'true' ]; then
+		$QUIET_ECHO "moved ${NEW_PATH_ITEM} to front of path." 1>&2
+		export PATH="${NEW_PATH_ITEM}:${PATH}"
 		return 0
 	fi
-	if [ ! "${to_back}" = 'true' ]; then
-		$quiet_echo "moved ${dir} to back of path." 1>&2
-		export PATH="${PATH}:${dir}"
+	if [ ! "${TO_BACK}" = 'true' ]; then
+		$QUIET_ECHO "moved ${NEW_PATH_ITEM} to back of path." 1>&2
+		export PATH="${PATH}:${NEW_PATH_ITEM}"
 		return 0
 	fi
 }
