fc037da6fdacd9db225752b0c0426f5aa1ca106e
iwantmyhatback <iwantmyhatbackk@gmail.com>
2023-11-25 18:12:27 -0500

    new brew location

--- .public_resources/applications.sh
+++ .public_resources/applications.sh
@@ -36,7 +36,6 @@ fi
 
 if command -v brew >/dev/null 2>&1; then
 	if [ "${SHELL}" = '/bin/zsh' ];then
-		HOMEBREW_PREFIX=/usr/local
 		# shellcheck disable=SC1091
 		[ -s "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ] && . "${HOMEBREW_PREFIX}/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 	fi
@@ -54,7 +53,7 @@ export JENV_LOADED=1
 unset JAVA_HOME
 unset JDK_HOME
 # shellcheck disable=SC1091
-. '/usr/local/Cellar/jenv/0.5.6/libexec/libexec/../completions/jenv.zsh'
+. "/opt/homebrew/Cellar/jenv/0.5.6/libexec/libexec/../completions/jenv.zsh"
 # shellcheck disable=SC2218
 jenv rehash 2>/dev/null
 # shellcheck disable=SC2218
@@ -62,20 +61,28 @@ jenv refresh-plugins
 jenv() {
 	# shellcheck disable=SC3044
 	type typeset > /dev/null 2>&1 && typeset command
-	command="$1"
+	command="${1}"
 	if [ "$#" -gt 0 ]; then
 		shift
 	fi
 
-	case "$command" in
+	case "${command}" in
 	enable-plugin|rehash|shell|shell-options)
 		# shellcheck disable=SC2046
-		eval $(jenv "sh-$command" "$@");;
+		eval $(jenv "sh-${command}" "$@");;
 	*)
-		command jenv "$command" "$@";;
+		command jenv "${command}" "$@";;
 	esac
 }
 
+
+###-----------------------------------------------------------------------------------------###
+###--- <NEW SECTION> -----------------------------------------------------------------------###
+###-----------------------------------------------------------------------------------------###
+
+export PYENV_ROOT="$HOME/.pyenv"
+[ -d "${PYENV_ROOT}/bin" ] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 
 ###-----------------------------------------------------------------------------------------###
 ###--- <NEW SECTION> -----------------------------------------------------------------------###
--- .public_resources/variables.sh
+++ .public_resources/variables.sh
@@ -18,11 +18,17 @@ export CLICOLOR=1 # Activate terminal colors
 ###-----------------------------------------------------------------------------------------###
 
 if command -v brew >/dev/null 2>&1; then
-	export HOMEBREW_PREFIX=/usr/local
-	export HOMEBREW_AUTOREMOVE=true
-	export HOMEBREW_CLEANUP_MAX_AGE_DAYS=7
-	export HOMEBREW_COLOR=true
-	export HOMEBREW_DISPLAY_INSTALL_TIMES=true
+  # From install (Output from: /opt/homebrew/bin/brew shellenv)
+  export HOMEBREW_REPOSITORY="/opt/homebrew"
+  export HOMEBREW_PREFIX="/opt/homebrew"
+  export HOMEBREW_CELLAR="${HOMEBREW_PREFIX}/Cellar"
+  export MANPATH="${HOMEBREW_PREFIX}/share/man${MANPATH+:$MANPATH}:"
+  export INFOPATH="${HOMEBREW_PREFIX}/share/info:${INFOPATH:-}"
+  # Added configurations
+  export HOMEBREW_AUTOREMOVE=true
+  export HOMEBREW_CLEANUP_MAX_AGE_DAYS=7
+  export HOMEBREW_COLOR=true
+  export HOMEBREW_DISPLAY_INSTALL_TIMES=true
 fi
 
 
